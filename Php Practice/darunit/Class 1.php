079fafda333ed316c4a894e3ec28d9f90bdad37e
Abu Zahin Mohammad Nowsin <abuzahinmohammadnowsin@gmail.com>
2023-10-10 17:01:28 +0600

    Darun IT PHP Class

---
+++ Php Practice/darunit/Class 1.php
@@ -0,0 +1,15 @@
+<?php
+    //Find Bigest and Smallest
+    $numone = 23;
+    $numtwo = 55;
+    $numthree = 100;
+
+    $bigestnum = max($numone, $numtwo, $numthree);
+
+    echo "Bigest number is " .$bigestnum;
+    echo "<br><br>";
+
+    $smallestnum = min($numone, $numtwo, $numthree);
+
+    echo "Smallest number is ".$smallestnum;
+
---
+++ Php Practice/darunit/Class 2.php
@@ -0,0 +1,37 @@
+<?php
+
+  $a = 10;
+  $b = 6;
+  echo $a**$b;
+  echo "<br>";
+
+
+
+  $marks = 60;
+
+  switch ($marks){
+    case 90:
+      echo "A";
+      break;
+    case 80:
+      echo "B";
+      break;
+    case 70:
+      echo "C";
+      break;
+    case 60:
+      echo "D";
+      break;
+    case 50:
+      echo "E";
+      break;
+    default:
+      echo "F";
+      break;
+  }
+  echo "<br>";
+
+  for($i = 1; $i <= 10; $i++){
+    echo $i . "<br>";
+  }
+  
